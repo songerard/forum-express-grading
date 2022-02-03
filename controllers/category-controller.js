0f766f4161dd1a6bef1138172524536f41fb39db
songerard <songerard@gmail.com>
2022-02-03 17:34:41 +0800

    feat: modify delete category

--- controllers/category-controller.js
+++ controllers/category-controller.js
@@ -1,4 +1,4 @@
-const { Category } = require('../models')
+const { Category, Restaurant } = require('../models')
 const categoryController = {
   getCategories: (req, res, next) => {
     return Promise.all([
@@ -29,9 +29,23 @@ const categoryController = {
       .catch(err => next(err))
   },
   deleteCategory: (req, res, next) => {
-    return Category.findByPk(req.params.id)
-      .then(category => {
+    return Promise.all([
+      Category.findOne({ where: { name: '(未分類)' } }),
+      Category.findByPk(req.params.id)
+    ])
+      .then(([notClassified, category]) => {
+        if (!notClassified) throw new Error('(未分類)尚未建立')
+        if (Number(req.params.id) === notClassified.id) throw new Error('(未分類)不能被刪除')
         if (!category) throw new Error("Category didn't exist!")
+        return Restaurant.update(
+          { categoryId: notClassified.id },
+          { where: { categoryId: req.params.id } }
+        )
+      })
+      .then(() => {
+        return Category.findByPk(req.params.id)
+      })
+      .then(category => {
         return category.destroy()
       })
       .then(() => res.redirect('/admin/categories'))
