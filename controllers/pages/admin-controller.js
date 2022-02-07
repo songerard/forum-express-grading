66ee37fde10457ca99a43f3c9e2719c039fa6f4b
songerard <songerard@gmail.com>
2022-02-07 20:54:45 +0800

    feat: add admin-services

--- controllers/pages/admin-controller.js
+++ controllers/pages/admin-controller.js
@@ -1,33 +1,10 @@
+const adminServices = require('../../services/admin-services')
 const { Restaurant, User, Category } = require('../../models')
 const { imgurFileHandler } = require('../../helpers/file-helpers')
 const { getOffset, getPagination } = require('../../helpers/pagination-helper')
 const adminController = {
   getRestaurants: (req, res, next) => {
-    const DEFAULT_LIMIT = 20
-    const categoryId = Number(req.query.categoryId) || ''
-    const page = Number(req.query.page) || 1
-    const limit = Number(req.query.limit) || DEFAULT_LIMIT
-    const offset = getOffset(limit, page)
-    return Promise.all([
-      Restaurant.findAndCountAll({
-        include: Category,
-        where: { ...categoryId ? { categoryId } : {} },
-        limit,
-        offset,
-        raw: true,
-        nest: true
-      }),
-      Category.findAll({ raw: true })
-    ])
-      .then(([restaurants, categories]) => {
-        return res.render('admin/restaurants', {
-          restaurants: restaurants.rows,
-          categories,
-          categoryId,
-          pagination: getPagination(limit, page, restaurants.count)
-        })
-      })
-      .catch(err => next(err))
+    adminServices.getRestaurants(req, (err, data) => err ? next(err) : res.render('admin/restaurants', data))
   },
   createRestaurant: (req, res, next) => {
     return Category.findAll({
