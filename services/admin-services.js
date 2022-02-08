7b6b6bd8aed54f2e72baab79e66ee7a398f1efc3
songerard <songerard@gmail.com>
2022-02-08 17:25:55 +0800

    feat: add back pagination and category in admin restaurants page

--- services/admin-services.js
+++ services/admin-services.js
@@ -1,11 +1,33 @@
 const { Restaurant, Category } = require('../models')
+const { imgurFileHandler } = require('../helpers/file-helpers')
+const { getOffset, getPagination } = require('../helpers/pagination-helper')
+
 const adminServices = {
   getRestaurants: (req, cb) => {
-    Restaurant.findAll({
-      raw: true,
-      nest: true,
-      include: [Category]
-    })
+    const DEFAULT_LIMIT = 20
+    const categoryId = Number(req.query.categoryId) || ''
+    const page = Number(req.query.page) || 1
+    const limit = Number(req.query.limit) || DEFAULT_LIMIT
+    const offset = getOffset(limit, page)
+    return Promise.all([
+      Restaurant.findAndCountAll({
+        include: Category,
+        where: { ...categoryId ? { categoryId } : {} },
+        limit,
+        offset,
+        raw: true,
+        nest: true
+      }),
+      Category.findAll({ raw: true })
+    ])
+      .then(([restaurants, categories]) => {
+        return cb(null, {
+          restaurants: restaurants.rows,
+          categories,
+          categoryId,
+          pagination: getPagination(limit, page, restaurants.count)
+        })
+      })
       .then(restaurants => cb(null, { restaurants }))
       .catch(err => cb(err))
   },
