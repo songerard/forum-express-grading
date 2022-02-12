1d5b4ce939c994c319c19e961ae81944d6d027e0
songerard <songerard@gmail.com>
2022-02-12 22:31:03 +0800

    feat: add 12 user api

--- controllers/pages/user-controller.js
+++ controllers/pages/user-controller.js
@@ -1,5 +1,3 @@
-const { User, Restaurant, Category, Comment, Favorite, Like, Followship, LikeComment } = require('../../models')
-const { imgurFileHandler } = require('../../helpers/file-helpers')
 const userServices = require('../../services/user-services')
 
 const userController = {
@@ -27,237 +25,45 @@ const userController = {
     res.redirect('/signin')
   },
   getUser: (req, res, next) => {
-    return User.findByPk(req.params.id, {
-      include: [
-        {
-          model: Comment,
-          include: { model: Restaurant, include: Category }
-        },
-        {
-          model: Restaurant,
-          as: 'FavoritedRestaurants',
-          include: Category
-        },
-        { model: Restaurant, as: 'LikedRestaurants' },
-        { model: User, as: 'Followers' },
-        { model: User, as: 'Followings' }
-      ]
-    })
-      .then(user => {
-        if (!user) throw new Error("User didn't exist!")
-
-        const result = user.toJSON()
-        // To reduce Comments to unique restaurant list:
-        // set initial accumulator is an empty array, then for each comment,
-        // if its restaurantId is found in accumulator then return empty array,
-        // otherwise return this comment and concat in accumulator for next iteration
-        // for the whole user.Comments
-        result.Comments = result.Comments.reduce((accumulator, currentComment) =>
-          accumulator.concat(accumulator.find(y => y.restaurantId === currentComment.restaurantId) ? [] : [currentComment]), [])
-
-        res.render('users/profile', {
-          user: result,
-          reqUser: req.user,
-          isFollowed: req.user.Followings.some(f => f.id === user.id)
-        })
-      })
-      .catch(err => next(err))
+    userServices.getUser(req, (err, data) => err ? next(err) : res.render('users/profile', data))
   },
   editUser: (req, res, next) => {
-    return User.findByPk(req.params.id, { raw: true })
-      .then(user => {
-        if (!user) throw new Error("User didn't exist!")
-        res.render('users/edit', { user })
-      })
-      .catch(err => next(err))
+    userServices.editUser(req, (err, data) => err ? next(err) : res.render('users/edit', data))
   },
   putUser: (req, res, next) => {
-    const { name } = req.body
-    if (!name) throw new Error('User name is required!')
-    User.findOne({ where: { name } })
-      .then(user => {
-        if (user) throw new Error('Name already exists!')
+    userServices.putUser(req, (err, data) => {
+      if (err) return next(err)
 
-        const { file } = req
-        return Promise.all([
-          User.findByPk(req.params.id),
-          imgurFileHandler(file)
-        ])
-          .then(([user, filePath]) => {
-            if (!user) throw new Error("User didn't exist!")
-            return user.update({
-              name,
-              image: filePath || user.image
-            })
-          })
-          .then(() => {
-            req.flash('success_messages', '使用者資料編輯成功')
-            res.redirect(`/users/${req.params.id}`)
-          })
-      })
-      .catch(err => next(err))
+      req.flash('success_messages', '使用者資料編輯成功')
+      res.redirect(`/users/${req.params.id}`)
+    })
   },
   addFavorite: (req, res, next) => {
-    const { restaurantId } = req.params
-    return Promise.all([
-      Restaurant.findByPk(restaurantId),
-      Favorite.findOne({
-        where: {
-          userId: req.user.id,
-          restaurantId
-        }
-      })
-    ])
-      .then(([restaurant, favorite]) => {
-        if (!restaurant) throw new Error("Restaurant didn't exist!")
-        if (favorite) throw new Error('You have favorited this restaurant!')
-
-        return Favorite.create({
-          userId: req.user.id,
-          restaurantId
-        })
-      })
-      .then(() => res.redirect('back'))
-      .catch(err => next(err))
+    userServices.addFavorite(req, (err, data) => err ? next(err) : res.redirect('back'))
   },
   removeFavorite: (req, res, next) => {
-    return Favorite.findOne({
-      where: {
-        userId: req.user.id,
-        restaurantId: req.params.restaurantId
-      }
-    })
-      .then(favorite => {
-        if (!favorite) throw new Error("You haven't favorited this restaurant")
-
-        return favorite.destroy()
-      })
-      .then(() => res.redirect('back'))
-      .catch(err => next(err))
+    userServices.removeFavorite(req, (err, data) => err ? next(err) : res.redirect('back'))
   },
   addLike: (req, res, next) => {
-    const { restaurantId } = req.params
-    return Restaurant.findByPk(restaurantId)
-      .then(restaurant => {
-        if (!restaurant) throw new Error("Restaurant didn't exist!")
-
-        return Like.findOrCreate({
-          where: {
-            userId: req.user.id,
-            restaurantId
-          }
-        })
-      })
-      .then(([like, created]) => {
-        if (!created) throw new Error("You have liked this restaurant!'")
-        res.redirect('back')
-      })
-      .catch(err => next(err))
+    userServices.addLike(req, (err, data) => err ? next(err) : res.redirect('back'))
   },
   removeLike: (req, res, next) => {
-    return Like.findOne({
-      where: {
-        userId: req.user.id,
-        restaurantId: req.params.restaurantId
-      }
-    })
-      .then(like => {
-        if (!like) throw new Error("You haven't liked this restaurant")
-
-        return like.destroy()
-      })
-      .then(() => res.redirect('back'))
-      .catch(next)
+    userServices.removeLike(req, (err, data) => err ? next(err) : res.redirect('back'))
   },
   getTopUsers: (req, res, next) => {
-    // 撈出所有 User 與 followers 資料
-    return User.findAll({
-      include: [{ model: User, as: 'Followers' }]
-    })
-      .then(users => {
-        // 整理 users 資料，把每個 user 項目都拿出來處理一次，並把新陣列儲存在 users 裡
-        const result = users
-          .map(user => ({
-            // 整理格式
-            ...user.toJSON(),
-            // 計算追蹤者人數
-            followerCount: user.Followers.length,
-            // 判斷目前登入使用者是否已追蹤該 user 物件
-            isFollowed: req.user.Followings.some(f => f.id === user.id)
-          }))
-          .sort((a, b) => b.followerCount - a.followerCount)
-        res.render('top-users', { users: result, reqUser: req.user })
-      })
-      .catch(err => next(err))
+    userServices.getTopUsers(req, (err, data) => err ? next(err) : res.render('top-users', data))
   },
   addFollowing: (req, res, next) => {
-    const { userId } = req.params
-    Promise.all([
-      User.findByPk(userId),
-      Followship.findOne({
-        where: {
-          followerId: req.user.id,
-          followingId: req.params.userId
-        }
-      })
-    ])
-      .then(([user, followship]) => {
-        if (!user) throw new Error("User didn't exist!")
-        if (followship) throw new Error('You are already following this user!')
-        return Followship.create({
-          followerId: req.user.id,
-          followingId: userId
-        })
-      })
-      .then(() => res.redirect('back'))
-      .catch(err => next(err))
+    userServices.addFollowing(req, (err, data) => err ? next(err) : res.redirect('back'))
   },
   removeFollowing: (req, res, next) => {
-    Followship.findOne({
-      where: {
-        followerId: req.user.id,
-        followingId: req.params.userId
-      }
-    })
-      .then(followship => {
-        if (!followship) throw new Error("You haven't followed this user!")
-        return followship.destroy()
-      })
-      .then(() => res.redirect('back'))
-      .catch(err => next(err))
+    userServices.removeFollowing(req, (err, data) => err ? next(err) : res.redirect('back'))
   },
   likeComment: (req, res, next) => {
-    return LikeComment.findOne({
-      where: {
-        userId: req.user.id,
-        commentId: req.params.commentId
-      },
-      raw: true
-    })
-      .then(like => {
-        if (like) throw new Error("You are already liked this comment!'")
-
-        return LikeComment.create({
-          userId: req.user.id,
-          commentId: req.params.commentId
-        })
-      })
-      .then(() => res.redirect(`/restaurants/${req.params.restaurantId}`))
-      .catch(err => next(err))
+    userServices.likeComment(req, (err, data) => err ? next(err) : res.redirect(`/restaurants/${req.params.restaurantId}`))
   },
   unlikeComment: (req, res, next) => {
-    LikeComment.findOne({
-      where: {
-        userId: req.user.id,
-        commentId: req.params.commentId
-      }
-    })
-      .then(like => {
-        if (!like) throw new Error("You haven't liked this comment!")
-        return like.destroy()
-      })
-      .then(() => res.redirect('back'))
-      .catch(err => next(err))
+    userServices.unlikeComment(req, (err, data) => err ? next(err) : res.redirect('back'))
   }
 }
 module.exports = userController
--- services/user-services.js
+++ services/user-services.js
@@ -1,5 +1,5 @@
 const bcrypt = require('bcryptjs')
-const { User } = require('../models')
+const { User, Restaurant, Category, Comment, Favorite, Like, Followship, LikeComment } = require('../models')
 const { imgurFileHandler } = require('../helpers/file-helpers')
 
 const userServices = {
@@ -33,6 +33,233 @@ const userServices = {
       .then(newUser => {
         return cb(null, { user: newUser })
       })
+      .catch(err => cb(err))
+  },
+  getUser: (req, cb) => {
+    return User.findByPk(req.params.id, {
+      include: [
+        {
+          model: Comment,
+          include: { model: Restaurant, include: Category }
+        },
+        {
+          model: Restaurant,
+          as: 'FavoritedRestaurants',
+          include: Category
+        },
+        { model: Restaurant, as: 'LikedRestaurants' },
+        { model: User, as: 'Followers' },
+        { model: User, as: 'Followings' }
+      ]
+    })
+      .then(user => {
+        if (!user) throw new Error("User didn't exist!")
+
+        const result = user.toJSON()
+        // To reduce Comments to unique restaurant list:
+        // set initial accumulator as an empty array, then for each comment,
+        // if its restaurantId is found in accumulator then return empty array,
+        // otherwise return this comment and concat in accumulator for next iteration
+        // for the whole user.Comments
+        result.Comments = result.Comments.reduce((accumulator, currentComment) =>
+          accumulator.concat(accumulator.find(y => y.restaurantId === currentComment.restaurantId) ? [] : [currentComment]), [])
+
+        return cb(null, {
+          user: result,
+          reqUser: req.user,
+          isFollowed: req.user.Followings.some(f => f.id === user.id)
+        })
+      })
+      .catch(err => cb(err))
+  },
+  editUser: (req, cb) => {
+    return User.findByPk(req.params.id, { raw: true })
+      .then(editedUser => {
+        if (!editedUser) throw new Error("User didn't exist!")
+        return cb(null, { user: editedUser })
+      })
+      .catch(err => cb(err))
+  },
+  putUser: (req, cb) => {
+    const { name } = req.body
+    if (!name) throw new Error('User name is required!')
+    User.findOne({ where: { name } })
+      .then(user => {
+        if (user) throw new Error('Name already exists!')
+
+        const { file } = req
+        return Promise.all([
+          User.findByPk(req.params.id),
+          imgurFileHandler(file)
+        ])
+          .then(([user, filePath]) => {
+            if (!user) throw new Error("User didn't exist!")
+            return user.update({
+              name,
+              image: filePath || user.image
+            })
+          })
+          .then(updatedUser => cb(null, { user: updatedUser }))
+      })
+      .catch(err => cb(err))
+  },
+  addFavorite: (req, cb) => {
+    const restaurantId = Number(req.params.restaurantId)
+    return Promise.all([
+      Restaurant.findByPk(restaurantId),
+      Favorite.findOne({
+        where: {
+          userId: req.user.id,
+          restaurantId
+        }
+      })
+    ])
+      .then(([restaurant, favorite]) => {
+        if (!restaurant) throw new Error("Restaurant didn't exist!")
+        if (favorite) throw new Error('You have favorited this restaurant!')
+
+        return Favorite.create({
+          userId: req.user.id,
+          restaurantId
+        })
+      })
+      .then(addedFavorite => cb(null, { favorite: addedFavorite }))
+      .catch(err => cb(err))
+  },
+  removeFavorite: (req, cb) => {
+    return Favorite.findOne({
+      where: {
+        userId: req.user.id,
+        restaurantId: req.params.restaurantId
+      }
+    })
+      .then(favorite => {
+        if (!favorite) throw new Error("You haven't favorited this restaurant")
+
+        return favorite.destroy()
+      })
+      .then(removedFavorite => cb(null, { favorite: removedFavorite }))
+      .catch(err => cb(err))
+  },
+  addLike: (req, cb) => {
+    const restaurantId = Number(req.params.restaurantId)
+    return Restaurant.findByPk(restaurantId)
+      .then(restaurant => {
+        if (!restaurant) throw new Error("Restaurant didn't exist!")
+
+        return Like.findOrCreate({
+          where: {
+            userId: req.user.id,
+            restaurantId
+          }
+        })
+      })
+      .then(([like, created]) => {
+        if (!created) throw new Error("You have liked this restaurant!'")
+        return cb(null, { like: like.toJSON() })
+      })
+      .catch(err => cb(err))
+  },
+  removeLike: (req, cb) => {
+    return Like.findOne({
+      where: {
+        userId: req.user.id,
+        restaurantId: req.params.restaurantId
+      }
+    })
+      .then(like => {
+        if (!like) throw new Error("You haven't liked this restaurant")
+
+        return like.destroy()
+      })
+      .then(removedLike => cb(null, { like: removedLike }))
+      .catch(err => cb(err))
+  },
+  getTopUsers: (req, cb) => {
+    return User.findAll({
+      include: [{ model: User, as: 'Followers' }]
+    })
+      .then(users => {
+        const result = users
+          .map(user => ({
+            ...user.toJSON(),
+            followerCount: user.Followers.length,
+            isFollowed: req.user.Followings.some(f => f.id === user.id)
+          }))
+          .sort((a, b) => b.followerCount - a.followerCount)
+        return cb(null, { users: result, reqUser: req.user })
+      })
+      .catch(err => cb(err))
+  },
+  addFollowing: (req, cb) => {
+    const userId = Number(req.params.userId)
+    Promise.all([
+      User.findByPk(userId),
+      Followship.findOne({
+        where: {
+          followerId: req.user.id,
+          followingId: req.params.userId
+        }
+      })
+    ])
+      .then(([user, followship]) => {
+        if (!user) throw new Error("User didn't exist!")
+        if (followship) throw new Error('You are already following this user!')
+        return Followship.create({
+          followerId: req.user.id,
+          followingId: userId
+        })
+      })
+      .then(addedFollowship => cb(null, { followship: addedFollowship }))
+      .catch(err => cb(err))
+  },
+  removeFollowing: (req, cb) => {
+    Followship.findOne({
+      where: {
+        followerId: req.user.id,
+        followingId: req.params.userId
+      }
+    })
+      .then(followship => {
+        if (!followship) throw new Error("You haven't followed this user!")
+        return followship.destroy()
+      })
+      .then(removedFollowship => cb(null, { followship: removedFollowship }))
+      .catch(err => cb(err))
+  },
+  likeComment: (req, cb) => {
+    const userId = req.user.id
+    const commentId = Number(req.params.commentId)
+    return LikeComment.findOne({
+      where: {
+        userId,
+        commentId
+      },
+      raw: true
+    })
+      .then(like => {
+        if (like) throw new Error("You are already liked this comment!'")
+
+        return LikeComment.create({
+          userId,
+          commentId
+        })
+      })
+      .then(likeComment => cb(null, { likeComment }))
+      .catch(err => cb(err))
+  },
+  unlikeComment: (req, cb) => {
+    LikeComment.findOne({
+      where: {
+        userId: req.user.id,
+        commentId: req.params.commentId
+      }
+    })
+      .then(like => {
+        if (!like) throw new Error("You haven't liked this comment!")
+        return like.destroy()
+      })
+      .then(unlikedComment => cb(null, { likeComment: unlikedComment }))
       .catch(err => cb(err))
   }
 }
