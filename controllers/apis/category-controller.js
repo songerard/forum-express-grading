d7124bc57f4310b0d1f2c6acf3452a7600d8cad4
songerard <songerard@gmail.com>
2022-02-12 16:19:37 +0800

    feat: add category api

---
+++ controllers/apis/category-controller.js
@@ -0,0 +1,17 @@
+const categoryServices = require('../../services/category-services')
+
+const categoryController = {
+  getCategories: (req, res, next) => {
+    categoryServices.getCategories(req, (err, data) => err ? next(err) : res.json(data))
+  },
+  postCategory: (req, res, next) => {
+    categoryServices.postCategory(req, (err, data) => err ? next(err) : res.json(data))
+  },
+  putCategory: (req, res, next) => {
+    categoryServices.putCategory(req, (err, data) => err ? next(err) : res.json(data))
+  },
+  deleteCategory: (req, res, next) => {
+    categoryServices.deleteCategory(req, (err, data) => err ? next(err) : res.json(data))
+  }
+}
+module.exports = categoryController
