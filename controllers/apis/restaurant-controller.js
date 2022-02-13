d5ffb3cafe65885f0a13e8e35548ef7076166062
songerard <songerard@gmail.com>
2022-02-13 17:01:27 +0800

    feat: add status success in some api response

--- controllers/apis/restaurant-controller.js
+++ controllers/apis/restaurant-controller.js
@@ -2,19 +2,19 @@ const restaurantServices = require('../../services/restaurant-services')
 
 const restaurantController = {
   getRestaurants: (req, res, next) => {
-    restaurantServices.getRestaurants(req, (err, data) => err ? next(err) : res.json(data))
+    restaurantServices.getRestaurants(req, (err, data) => err ? next(err) : res.json({ status: 'success', data }))
   },
   getRestaurant: (req, res, next) => {
-    restaurantServices.getRestaurant(req, (err, data) => err ? next(err) : res.json(data))
+    restaurantServices.getRestaurant(req, (err, data) => err ? next(err) : res.json({ status: 'success', data }))
   },
   getDashboard: (req, res, next) => {
-    restaurantServices.getDashboard(req, (err, data) => err ? next(err) : res.json(data))
+    restaurantServices.getDashboard(req, (err, data) => err ? next(err) : res.json({ status: 'success', data }))
   },
   getFeeds: (req, res, next) => {
-    restaurantServices.getFeeds(req, (err, data) => err ? next(err) : res.json(data))
+    restaurantServices.getFeeds(req, (err, data) => err ? next(err) : res.json({ status: 'success', data }))
   },
   getTopRestaurants: (req, res, next) => {
-    restaurantServices.getTopRestaurants(req, (err, data) => err ? next(err) : res.json(data))
+    restaurantServices.getTopRestaurants(req, (err, data) => err ? next(err) : res.json({ status: 'success', data }))
   }
 }
 module.exports = restaurantController
